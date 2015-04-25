6da27ad649b47a64f4e1062507c1e1588bb9b8a8
techjanitor <puntme@gmail.com>
2015-04-25 01:08:43 -0500

    add tag info handler

---
+++ controllers/taginfo.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+
+	e "github.com/techjanitor/pram-get/errors"
+	"github.com/techjanitor/pram-get/models"
+)
+
+// TagInfoController handles tag info
+func TagInfoController(c *gin.Context) {
+
+	// Get parameters from validate middleware
+	params := c.MustGet("params").([]uint)
+
+	// Initialize model struct
+	m := &models.TagInfoModel{
+		Id: params[0],
+	}
+
+	// Get the model which outputs JSON
+	err := m.Get()
+	if err == e.ErrNotFound {
+		c.Set("controllerError", err)
+		c.JSON(e.ErrorMessage(e.ErrNotFound))
+		c.Error(err, "Operation aborted")
+		return
+	}
+	if err != nil {
+		c.Set("controllerError", err)
+		c.JSON(e.ErrorMessage(e.ErrInternalError))
+		c.Error(err, "Operation aborted")
+		return
+	}
+
+	// Marshal the structs into JSON
+	output, err := json.Marshal(m.Result)
+	if err != nil {
+		c.Set("controllerError", err)
+		c.JSON(e.ErrorMessage(e.ErrInternalError))
+		c.Error(err, "Operation aborted")
+		return
+	}
+
+	// Hand off data to cache middleware
+	c.Set("data", output)
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.Write(output)
+
+	return
+
+}
--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 	controllers.GET("/index/:ib/:page", c.IndexController)
 	controllers.GET("/thread/:thread/:page", c.ThreadController)
 	controllers.GET("/tag/:tag/:page", c.TagController)
+	controllers.GET("/tag/:id", c.TagInfoController)
 	controllers.GET("/directory/:ib", c.DirectoryController)
 	controllers.GET("/image/:id", c.ImageController)
 	controllers.GET("/post/:thread/:id", c.PostController)
