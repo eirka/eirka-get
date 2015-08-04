450bca2137b33bf9770f5d0b20b0f5fbb9d3fa1d
techjanitor <puntme@gmail.com>
2015-08-04 01:09:27 -0500

    add skip key to analytics

--- middleware/analytics.go
+++ middleware/analytics.go
@@ -33,11 +33,19 @@ func Analytics() gin.HandlerFunc {
 		// Process request
 		c.Next()
 
+		// Stop timer
+		end := time.Now()
+		// get request latency
+		latency := end.Sub(start)
+
 		// get userdata from session middleware
 		userdata := c.MustGet("userdata").(u.User)
 
 		// get cached state from cache middleware
 		cached := c.MustGet("cached").(bool)
+
+		// get the ib
+		ib := c.Param("ib")
 
 		// Trim leading / from path and split
 		params := strings.Split(strings.Trim(path, "/"), "/")
@@ -46,49 +54,45 @@ func Analytics() gin.HandlerFunc {
 		key := itemKey{}
 		key.generateKey(params...)
 
-		// get the ib
-		ib := c.Param("ib")
+		if !skipKey(params[0]) {
 
-		// Stop timer
-		end := time.Now()
-		// get request latency
-		latency := end.Sub(start)
+			// set our data
+			request := RequestType{
+				Ib:        ib,
+				Ip:        c.ClientIP(),
+				User:      userdata.Id,
+				Path:      path,
+				Status:    c.Writer.Status(),
+				Latency:   latency,
+				ItemKey:   key.Key,
+				ItemValue: key.Value,
+				Cached:    cached,
+			}
 
-		// set our data
-		request := RequestType{
-			Ib:        ib,
-			Ip:        c.ClientIP(),
-			User:      userdata.Id,
-			Path:      path,
-			Status:    c.Writer.Status(),
-			Latency:   latency,
-			ItemKey:   key.Key,
-			ItemValue: key.Value,
-			Cached:    cached,
-		}
+			// Get Database handle
+			db, err := u.GetDb()
+			if err != nil {
+				c.Error(err)
+				c.Abort()
+				return
+			}
 
-		// Get Database handle
-		db, err := u.GetDb()
-		if err != nil {
-			c.Error(err)
-			c.Abort()
-			return
-		}
+			// prepare query for analytics table
+			ps1, err := db.Prepare("INSERT INTO analytics (ib_id, user_id, request_ip, request_path, request_status, request_latency, request_itemkey, request_itemvalue, request_cached, request_time) VALUES (?,?,?,?,?,?,?,?,?,NOW())")
+			if err != nil {
+				c.Error(err)
+				c.Abort()
+				return
+			}
 
-		// prepare query for analytics table
-		ps1, err := db.Prepare("INSERT INTO analytics (ib_id, user_id, request_ip, request_path, request_status, request_latency, request_itemkey, request_itemvalue, request_cached, request_time) VALUES (?,?,?,?,?,?,?,?,?,NOW())")
-		if err != nil {
-			c.Error(err)
-			c.Abort()
-			return
-		}
+			// input data
+			_, err = ps1.Exec(request.Ib, request.User, request.Ip, request.Path, request.Status, request.Latency, request.ItemKey, request.ItemValue, request.Cached)
+			if err != nil {
+				c.Error(err)
+				c.Abort()
+				return
+			}
 
-		// input data
-		_, err = ps1.Exec(request.Ib, request.User, request.Ip, request.Path, request.Status, request.Latency, request.ItemKey, request.ItemValue, request.Cached)
-		if err != nil {
-			c.Error(err)
-			c.Abort()
-			return
 		}
 
 	}
@@ -112,5 +116,21 @@ func (r *itemKey) generateKey(params ...string) {
 	}
 
 	return
+
+}
+
+// Check if key should be expired
+func skipKey(key string) bool {
+
+	keyList := map[string]bool{
+		"taginfo": true,
+		"tags":    true,
+	}
+
+	if keyList[strings.ToLower(key)] {
+		return true
+	}
+
+	return false
 
 }
