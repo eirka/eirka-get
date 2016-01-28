70d3a604f673ad1a8fc430cab04ec47250f7a2ba
techjanitor <puntme@gmail.com>
2016-01-27 22:00:31 -0600

    fix up redis lib

--- middleware/cache.go
+++ middleware/cache.go
@@ -7,31 +7,6 @@ import (
 	"github.com/eirka/eirka-libs/redis"
 )
 
-var (
-	RedisKeyIndex = make(map[string]RedisKey)
-	RedisKeys     = []RedisKey{
-		{base: "index", fieldcount: 1, hash: true, expire: false},
-		{base: "thread", fieldcount: 2, hash: true, expire: false},
-		{base: "tag", fieldcount: 2, hash: true, expire: true},
-		{base: "image", fieldcount: 1, hash: true, expire: false},
-		{base: "post", fieldcount: 2, hash: true, expire: false},
-		{base: "tags", fieldcount: 1, hash: true, expire: false},
-		{base: "directory", fieldcount: 1, hash: true, expire: false},
-		{base: "new", fieldcount: 1, hash: false, expire: true},
-		{base: "popular", fieldcount: 1, hash: false, expire: true},
-		{base: "favorited", fieldcount: 1, hash: false, expire: true},
-		{base: "tagtypes", fieldcount: 0, hash: false, expire: false},
-		{base: "imageboards", fieldcount: 0, hash: false, expire: true},
-	}
-)
-
-func init() {
-	// key index map
-	for _, key := range RedisKeys {
-		RedisKeyIndex[key.base] = key
-	}
-}
-
 // Cache will check for the key in Redis and serve it. If not found, it will
 // take the marshalled JSON from the controller and set it in Redis
 func Cache() gin.HandlerFunc {
@@ -50,7 +25,7 @@ func Cache() gin.HandlerFunc {
 		params := strings.Split(strings.Trim(c.Request.URL.Path, "/"), "/")
 
 		// get the keyname
-		key, ok := RedisKeyIndex[params[0]]
+		key, ok := redis.RedisKeyIndex[params[0]]
 		if !ok {
 			c.Next()
 			return
@@ -96,60 +71,4 @@ func Cache() gin.HandlerFunc {
 		return
 	}
 
-}
-
-type RedisKey struct {
-	base       string
-	fieldcount int
-	hash       bool
-	expire     bool
-	key        string
-	hashid     string
-}
-
-func (r *RedisKey) SetKey(ids ...string) {
-
-	if r.fieldcount == 0 {
-		r.key = r.base
-		return
-	}
-
-	// create our key
-	r.key = strings.Join([]string{r.base, strings.Join(ids[:r.fieldcount], ":")}, ":")
-
-	// get our hash id
-	if r.hash {
-		r.hashid = strings.Join(ids[r.fieldcount:], "")
-	}
-
-	return
-}
-
-func (r *RedisKey) Get() (result []byte, err error) {
-
-	if r.hash {
-		return redis.RedisCache.HGet(r.key, r.hashid)
-	} else {
-		return redis.RedisCache.Get(r.key)
-	}
-
-	return
-}
-
-func (r *RedisKey) Set(data []byte) (err error) {
-
-	if r.hash {
-		err = redis.RedisCache.HMSet(r.key, r.hashid, data)
-	} else {
-		err = redis.RedisCache.Set(r.key, data)
-	}
-	if err != nil {
-		return
-	}
-
-	if r.expire {
-		return redis.RedisCache.Expire(r.key, 600)
-	}
-
-	return
 }
